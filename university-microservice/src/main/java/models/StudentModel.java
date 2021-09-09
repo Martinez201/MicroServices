9d7ddb26e8020f859711d018acda847577285014
Jesus <jesus.martinez.gonzalez1993@gmail.com>
2021-09-09 13:56:13 +0200

    añadida modelos de student y course

---
+++ university-microservice/src/main/java/models/StudentModel.java
@@ -0,0 +1,21 @@
+package models;
+
+
+import lombok.*;
+import org.springframework.hateoas.RepresentationModel;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+public class StudentModel extends RepresentationModel<StudentModel> {
+
+    private Long id;
+    private String name;
+    private String passportNumber;
+
+    private List<CourseModel> courses;
+}
